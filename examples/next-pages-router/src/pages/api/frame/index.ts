15c21e801ad8bba842144907a2dda66f11c115de
paigexx <pcjones12@gmail.com>
2024-02-14 16:52:40 -0600

    updates img

--- examples/next-pages-router/src/pages/api/frame/index.ts
+++ examples/next-pages-router/src/pages/api/frame/index.ts
@@ -18,19 +18,52 @@ export default async function handler(
   // The GET request is the intial request to the frame from index.tsx.
   if (req.method === "GET") {
     try {
-    const frameMetadata = await fdk.getFrameMetadata({input: {text: "Send Pinnie a Valentine"}, buttons: [{label: "Send"}], cid: "QmYGS65BsDE7D4yw342GbGjF5LYgf73KnFXoYSwuSY7qL7", post_url: `${process.env.HOSTED_DOMAIN}/api/frame`,
+    const frameMetadata = await fdk.getFrameMetadata({input: {text: "Send Pinnie a Valentine"}, buttons: [{label: "Send"}], cid: "QmPsnQz3RCZvQZdjXQ1GQK6kJAMc289CCJr9DqS8MexUrU", post_url: `${process.env.HOSTED_DOMAIN}/api/frame`,
   })
     const html = 
     `<!DOCTYPE html>
     <html lang="en">
       <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         ${frameMetadata}
+        <title>Send Pinnie a Valentine</title>
+        <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #800080; /* Purple background */
+            background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 80 40"><text x="0" y="20" font-size="24">💗</text></svg>'); /* Pink heart */
+            background-repeat: repeat;
+        }
+        .box {
+            height: 300px;
+            text-align: center;
+            padding: 10px;
+            animation: spin 20s linear infinite; /* Spin animation */
+        }
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+        h1 {
+            color: #ff00ff; /* Pink text color */
+        }
+    </style>
       </head>
       <body>
-        <div style="background: pink;">
-          <h1>Send Pinnie a Valentine</h1>
-        </div>
-      </body>
+      <div class="box">
+        <img src=https://${process.env.PINATA_GATEWAY}/ipfs/QmPsnQz3RCZvQZdjXQ1GQK6kJAMc289CCJr9DqS8MexUrU alt="Valentine's Day Image" width="70%" height="auto">
+      </div>
+    </body>
       </html>`
     res.send(html);
     } catch (error) {
