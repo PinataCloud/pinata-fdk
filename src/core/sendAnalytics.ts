1079c8ce41481fce123d7125749869a06005e99e
paigexx <pcjones12@gmail.com>
2024-02-22 12:13:29 -0600

    makes custom_id conditional
    Release version 3.3.2
    
    changelog

--- src/core/sendAnalytics.ts
+++ src/core/sendAnalytics.ts
@@ -1,19 +1,32 @@
 import { FrameActionPayload, PinataConfig} from './types';
 
+type DataObject = {
+    data: { messageBytes: string };
+    frame_id: string;
+    custom_id?: string; // Make custom_id optional
+}
+
 
 /**
  * This function sends frame data to track analytics.
  * @param frame_id: The id representing the frame.
- * @param frame_data: The Frame Action data produced by Farcasater (req.body)
- * @param custom_id: (Optional) 
+ * @param frame_data: The Frame Action data produced by Farcasater.
+ * @param custom_id: A unique identifier to segment requests within the specified frame (Optional) 
  * @returns Success message boolean.
  */
 
 export async function sendAnalytics(frame_id: string, frame_data: FrameActionPayload, config: PinataConfig | undefined, custom_id?: string) {
     if(!config){
       throw new Error('Pinata configuration required to send analytics.')
-    }    
-    
+    } 
+    const data: DataObject = {
+        data: frame_data.trustedData,
+        frame_id: frame_id,
+    } 
+    if (custom_id) {
+        data.custom_id = custom_id;
+    }
+
     try {
         const result = await fetch(
             "https://api.pinata.cloud/farcaster/frames/interactions",
@@ -23,11 +36,7 @@ export async function sendAnalytics(frame_id: string, frame_data: FrameActionPay
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${config?.pinata_jwt}`
                 },
-                body: JSON.stringify({
-                    data: frame_data,
-                    frame_id: frame_id,
-                    custom_id: custom_id
-                }),
+                body: JSON.stringify(data),
             }
         );
 
