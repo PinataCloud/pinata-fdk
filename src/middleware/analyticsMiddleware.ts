682ed320df23c58aeef6dd1d36589812cf089888
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-29 11:26:39 -0700

    fixed ordering of function arguments

--- src/middleware/analyticsMiddleware.ts
+++ src/middleware/analyticsMiddleware.ts
@@ -2,19 +2,19 @@ import { PinataConfig } from "../core/types";
 import { sendAnalytics } from "../core/sendAnalytics";
 
 
-export async function analyticsMiddleware(context: any, frameId: string, config: PinataConfig, next: any, customId?: string) {
-    if(!config){
+export async function analyticsMiddleware(context: any, frameId: string, next: any, config: PinataConfig | undefined, customId?: string) {
+    if (!config) {
         throw new Error('Pinata configuration required to send analytics.')
-      } 
+    }
     try {
         if (context.req.method === "POST") {
             const body = await context.req.json();
-            const status = await sendAnalytics(frameId, body, config, customId);
-            return status;
+            await sendAnalytics(frameId, body, config, customId);
         }
-    await next();
+        await next();
     } catch (error) {
-     console.log(error) 
+        console.log(error)
+        return context.res.send("Error with analytics");
     }
-       
+
 }
