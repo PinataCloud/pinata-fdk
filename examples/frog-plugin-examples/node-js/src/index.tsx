0b59d14907c7290be3eece7e224ce8eaae619b83
paigexx <pcjones12@gmail.com>
2024-03-05 17:18:10 -0600

    adds frog plugin example templates

---
+++ examples/frog-plugin-examples/node-js/src/index.tsx
@@ -0,0 +1,77 @@
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { Button, Frog, TextInput } from 'frog'
+import {PinataFDK} from 'pinata-fdk'
+import { config } from 'dotenv'
+config()
+
+
+export const app = new Frog({
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: "https://hub.pinata.cloud"
+})
+
+export const fdk = new PinataFDK({ pinata_jwt: process.env.PINATA_JWT!, pinata_gateway: process.env.PINATA_GATEWAY! })
+app.use("/", fdk.analyticsMiddleware({ frameId: "frog-node-example", customId: "frog-node-example-custom"}));
+
+app.use('/*', serveStatic({ root: './public' }))
+
+
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button value="bananas">Bananas</Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port,
+})
---
+++ examples/frog-plugin-examples/vercel/api/index.tsx
@@ -0,0 +1,73 @@
+import { Button, Frog, TextInput } from 'frog'
+import { handle } from 'frog/vercel'
+import { PinataFDK } from 'pinata-fdk'
+import { config } from 'dotenv'
+config()
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub API URL to enable frame verification.
+  // hubApiUrl: "https://hub.pinata.cloud"
+})
+
+export const fdk = new PinataFDK({ pinata_jwt: process.env.PINATA_JWT!, pinata_gateway: process.env.PINATA_GATEWAY! })
+app.use("/", fdk.analyticsMiddleware({ frameId: "frog-vercel-example", customId: "frog-vercel-example-custom"}));
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button value="bananas">Bananas</Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
