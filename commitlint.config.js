c4af5816c7d703c8ad59568e1e4163b8b78951f2
paigexx <pcjones12@gmail.com>
2024-04-24 10:18:51 -0500

    chore: updates commit linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'feat',
+          'fix',
+          'docs',
+          'chore',
+          'style',
+          'refactor',
+          'ci',
+          'test',
+          'revert',
+          'perf',
+          'vercel',
+        ],
+      ],
+    },
+};
