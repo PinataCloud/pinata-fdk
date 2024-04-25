3e6b14a70e8dce5dbf7c8fd5e11f0f2ba30a357f
paigexx <pcjones12@gmail.com>
2024-04-25 18:00:38 -0500

    chore: adds user write tests

---
+++ tests/pinata/users/followUser.test.ts
@@ -0,0 +1,67 @@
+import { PinataConfig } from "../../../src";
+import { followUser } from "../../../src/core/pinata/users/followUser";
+
+describe('followUser function', () => {
+  let originalFetch: typeof fetch;
+
+  beforeEach(() => {
+    originalFetch = global.fetch;
+  });
+
+  afterEach(() => {
+    global.fetch = originalFetch;
+  });
+
+  it('should follow a user', async () => {
+    const mockConfig: PinataConfig = { pinata_jwt: 'test_jwt', pinata_gateway: "example.cloud"};
+    const mockRequest = { fid: 3, signerId: 'test_signerId' };
+    const mockResponse = {
+      data: {
+          type: "123",
+          fid: 3,
+          timestamp: 3,
+          network: "123",
+          castAddBody: {
+          embedsDeprecated: [],
+          mentions: [],
+          text: "123",
+          mentionsPositions: [],
+          embeds: [],
+          },
+      },
+      hash: "123",
+      hashScheme: "123",
+      signature: "123",
+      signatureScheme: "123",
+      signer: "123",
+      dataBytes: "123"
+    };
+
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockResponse),
+    });
+
+    const result = await followUser(mockRequest, mockConfig);
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      `https://api.pinata.cloud/v3/farcaster/follow/${mockRequest.fid}?signerId=${mockRequest.signerId}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${mockConfig.pinata_jwt}`,
+        },
+      }
+    );
+    expect(result).toEqual(mockResponse.data);
+  });
+
+  it('should throw error if fetch fails', async () => {
+    const mockConfig: PinataConfig = { pinata_jwt: 'test_jwt', pinata_gateway: "example.cloud"};
+    const mockRequest = { fid: 3, signerId: 'test_signerId' };
+    const mockError = new Error('Failed to follow user');
+    global.fetch = jest.fn().mockRejectedValueOnce(mockError);
+
+    await expect(followUser(mockRequest, mockConfig)).rejects.toThrow(mockError);
+  });
+});
